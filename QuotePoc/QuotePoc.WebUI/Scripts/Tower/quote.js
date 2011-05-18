be90bf5b364971ff840b417f0c0fd1ed6880f485
Tatham Oddie <tatham@oddie.com.au>
2011-05-18 17:09:01 +1000

    Moved view models out into the global namespace for now.

--- QuotePoc/QuotePoc.WebUI/Scripts/Tower/quote.js
+++ QuotePoc/QuotePoc.WebUI/Scripts/Tower/quote.js
@@ -1,39 +1,31 @@
 ﻿/// <reference path="../jquery-1.5.1.js" />
 /// <reference path="../knockout-1.2.0.js" />
 
-var quoteInterfaceManager = function () {
-
-    var lifeViewModel = function () {
-        var vm = {
-            title: ko.observable(""),
-            firstName: ko.observable(""),
-            lastName: ko.observable(""),
-            dateOfBirth: ko.observable("")
-        };
-        vm.ageNext = ko.dependentObservable(function () {
-            var parsedDateOfBirth = new Date(vm.dateOfBirth());
-            var years = new Date().getFullYear() - parsedDateOfBirth.getFullYear();
-            return isNaN(years) ? '' : years;
-        });
-        return vm;
+var lifeViewModel = function () {
+    var vm = {
+        title: ko.observable(""),
+        firstName: ko.observable(""),
+        lastName: ko.observable(""),
+        dateOfBirth: ko.observable("")
     };
+    vm.ageNext = ko.dependentObservable(function () {
+        var parsedDateOfBirth = new Date(vm.dateOfBirth());
+        var years = new Date().getFullYear() - parsedDateOfBirth.getFullYear();
+        return isNaN(years) ? '' : years;
+    });
+    return vm;
+};
 
-    var quoteViewModel = function () {
-        var vm = {
-            lives: new ko.observableArray()
-        };
-        vm.addLife = function () {
-            vm.lives.push(new lifeViewModel());
-        };
-        return vm;
+var quoteViewModel = function () {
+    var vm = {
+        lives: new ko.observableArray()
     };
-
-    return {
-        init: function () {
-            ko.applyBindings(new quoteViewModel());
-        }
+    vm.addLife = function () {
+        vm.lives.push(new lifeViewModel());
     };
+    return vm;
+};
 
-} ();
-
-$(quoteInterfaceManager.init);
+$(function () {
+    ko.applyBindings(new quoteViewModel());
+});
