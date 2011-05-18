1a6f657c2d425d8f47f946155b7b20659adefb9d
Tatham Oddie <tatham@oddie.com.au>
2011-05-18 16:10:11 +1000

    Added JSON output on POST for debugging.

--- QuotePoc/QuotePoc.WebUI/Controllers/QuoteController.cs
+++ QuotePoc/QuotePoc.WebUI/Controllers/QuoteController.cs
@@ -1,13 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System.Net.Mime;
+using System.Web.Mvc;
 using QuotePoc.WebUI.Models;
 
 namespace QuotePoc.WebUI.Controllers
 {
     public class QuoteController : Controller
     {
+        [HttpGet]
         public ActionResult Quote()
         {
             return View(new Quote());
+        }
+
+        [HttpPost]
+        public ActionResult Quote(Quote quote)
+        {
+            return Json(quote, MediaTypeNames.Text.Plain, JsonRequestBehavior.AllowGet);
         }
     }
 }
