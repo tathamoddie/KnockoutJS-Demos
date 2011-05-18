73968d655ffbd47c5664e6e00f93bd1c705d31f2
Tatham Oddie <tatham@oddie.com.au>
2011-05-18 16:03:20 +1000

    Implemented basic template for lives.

--- QuotePoc/QuotePoc.WebUI/Scripts/Tower/quote.js
+++ QuotePoc/QuotePoc.WebUI/Scripts/Tower/quote.js
@@ -1,3 +1,21 @@
 ﻿/// <reference path="../jquery-1.5.1.js" />
 /// <reference path="../knockout-1.2.0.js" />
 
+var quoteInterfaceManager = function () {
+
+    var viewModel = {
+        lives: new ko.observableArray([
+            { title: "Mr", firstName: "Tatham", lastName: "Oddie" },
+            { title: "Mr", firstName: "Jake", lastName: "Robson" }
+        ])
+    };
+
+    return {
+        init: function () {
+            ko.applyBindings(viewModel);
+        }
+    };
+
+}();
+
+$(quoteInterfaceManager.init);
