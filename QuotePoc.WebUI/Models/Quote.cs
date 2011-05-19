3ecc3a3297b94f3ce367765435123fa95d6a5cdf
Tatham Oddie <tatham@oddie.com.au>
2011-05-19 12:12:43 +1000

    Added a validation rule on the quote object.

--- QuotePoc.WebUI/Models/Quote.cs
+++ QuotePoc.WebUI/Models/Quote.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace QuotePoc.WebUI.Models
 {
-    public class Quote
+    public class Quote : IValidatableObject
     {
         public IEnumerable<Life> Lives { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Lives != null &&
+                !Lives.Any(l => l.FirstName.Equals("Tom", StringComparison.OrdinalIgnoreCase)))
+                yield return new ValidationResult("At least one person must be named Tom", new[] {"Lives"});
+        }
     }
 }
