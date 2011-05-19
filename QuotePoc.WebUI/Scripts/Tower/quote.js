8f1085730b331c64f9258733eacf4f689d996760
Tatham Oddie <tatham@oddie.com.au>
2011-05-19 12:01:30 +1000

    Started pulling framework methods into their own JS file.

--- QuotePoc.WebUI/Scripts/Tower/quote.js
+++ QuotePoc.WebUI/Scripts/Tower/quote.js
@@ -1,12 +1,6 @@
 ﻿/// <reference path="../jquery-1.5.1.js" />
 /// <reference path="../knockout-1.2.0.js" />
-/// <reference path="../json2.js" />
-
-var refreshValidation = function () {
-    $("form").removeData("validator");
-    $("form").removeData("unobtrusiveValidation");
-    $.validator.unobtrusive.parse("form");
-};
+/// <reference path="site.js" />
 
 var lifeViewModel = function () {
     var vm = {
@@ -38,24 +32,5 @@ var quoteViewModel = function () {
 };
 
 $(function () {
-    $(".quote-interface").each(function () {
-        var viewModel = new quoteViewModel();
-        var initialJson = $(this).find("script.knockout-initial-data").html();
-        if (initialJson.length > 0) {
-            var initialData = JSON.parse(initialJson);
-            if (initialData.Lives) {
-                var lifeCount = initialData.Lives.length;
-                for (var i = 0; i < lifeCount; i++) {
-                    var life = initialData.Lives[i];
-                    var lifeVM = new lifeViewModel();
-                    lifeVM.title(life.Title);
-                    lifeVM.firstName(life.FirstName);
-                    lifeVM.lastName(life.LastName);
-                    lifeVM.dateOfBirth(life.DateOfBirth);
-                    viewModel.lives.push(lifeVM);
-                }
-            }
-        };
-        ko.applyBindings(viewModel, this);
-    });
+    applyKnockout($(".quote-interface"), quoteViewModel);
 });
---
+++ QuotePoc.WebUI/Scripts/Tower/site.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../jquery-1.5.1.js" />
+/// <reference path="../knockout-1.2.0.js" />
+/// <reference path="../json2.js" />
+
+var refreshValidation = function () {
+    $("form").removeData("validator");
+    $("form").removeData("unobtrusiveValidation");
+    $.validator.unobtrusive.parse("form");
+};
+
+var applyKnockout = function (target, viewModelType) {
+    $(target).each(function () {
+        var viewModel = new viewModelType();
+        var initialJson = $(this).find("script.knockout-initial-data").html();
+        if (initialJson.length > 0) {
+            var initialData = JSON.parse(initialJson);
+            if (initialData.Lives) {
+                var lifeCount = initialData.Lives.length;
+                for (var i = 0; i < lifeCount; i++) {
+                    var life = initialData.Lives[i];
+                    var lifeVM = new lifeViewModel();
+                    lifeVM.title(life.Title);
+                    lifeVM.firstName(life.FirstName);
+                    lifeVM.lastName(life.LastName);
+                    lifeVM.dateOfBirth(life.DateOfBirth);
+                    viewModel.lives.push(lifeVM);
+                }
+            }
+        };
+        ko.applyBindings(viewModel, this);
+    });
+};
