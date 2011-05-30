d0cb9220466ae176b35adcc3a6e00a8e930f7733
Tatham Oddie <tatham@oddie.com.au>
2011-05-30 23:22:06 +1000

    Moved lifeViewModel into its own JS file.

--- QuotePoc.WebUI/Scripts/QuotePoc/quote.js
+++ QuotePoc.WebUI/Scripts/QuotePoc/quote.js
@@ -1,37 +1,7 @@
 ﻿/// <reference path="../jquery-1.5.1.js" />
 /// <reference path="../knockout-1.2.0.js" />
 /// <reference path="site.js" />
-
-var lifeViewModel = function () {
-    var vm = {
-        title: ko.observable(""),
-        firstName: ko.observable(""),
-        lastName: ko.observable(""),
-        dateOfBirth: ko.observable("")
-    };
-    vm.formattedDateOfBirth = ko.dependentObservable({
-        read: function () {
-            var dateOfBirth = vm.dateOfBirth();
-            var isValidDate = Object.prototype.toString.call(dateOfBirth) === "[object Date]" && !isNaN(dateOfBirth.getTime());
-            return isValidDate
-                ? dateOfBirth.getDate() + '/' + (dateOfBirth.getMonth() + 1) + '/' + dateOfBirth.getFullYear()
-                : '';
-        },
-        write: function (value) {
-            var d = /(\d{1,2})\/(\d{1,2})\/(\d{4})/.exec(value);
-            if (d) {
-                vm.dateOfBirth(new Date(+d[3], +d[2]-1, +d[1]));
-            }
-        },
-        owner: vm
-    });
-    vm.ageNext = ko.dependentObservable(function () {
-        var parsedDateOfBirth = new Date(vm.dateOfBirth());
-        var years = new Date().getFullYear() - parsedDateOfBirth.getFullYear();
-        return isNaN(years) ? '' : years;
-    });
-    return vm;
-};
+/// <reference path="life.js" />
 
 var quoteViewModel = function () {
     var vm = {
